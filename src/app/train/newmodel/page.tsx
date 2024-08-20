21bead96e6f15317e28b04c6bf611d89a9e1724a
Hassan-jr <abdiladifhassan115@gmail.com>
2024-08-20 12:48:56 +0300

    v6.2

--- src/app/train/newmodel/page.tsx
+++ src/app/train/newmodel/page.tsx
@@ -1,20 +1,18 @@
 "use client";
 import TrainImgUpload from "../uploadImgs";
 import React from "react";
-import { Wizard } from "react-use-wizard";
 import DataFormat from "../data";
 import Caption from "../caption";
 import SubmitTrain from "../submitTrain";
-import StepperFooter from "../Footer";
 
 const Home = () => {
   return (
-    <Wizard footer={<StepperFooter />}>
+    <div>
       <DataFormat />
       <TrainImgUpload />
       <Caption />
       <SubmitTrain />
-    </Wizard>
+    </div>
   );
 };
 
