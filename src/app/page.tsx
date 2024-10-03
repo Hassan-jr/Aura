d3d1903b6a964257590623ecd7120c84d050ec7f
Hassan-jr <abdiladifhassan115@gmail.com>
2024-10-03 12:46:36 +0300

    landing page added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,62 @@
-import Image from "next/image";
+"use client";
+import { LandingHeader } from "@/components/landing/header";
+import { ImageTree } from "@/components/landing/imgtree";
+import Particles from "@/components/ui/particles";
+import { NavigationMenu } from "@/customui/navbar";
+import { ModeToggle } from "@/customui/themebtn";
+import Meteors from "@/components/ui/meteors";
+
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const lightTheme =
+    "absolute top-0 -z-10 h-auto w-screen bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]";
+  const darkTheme =
+    "absolute top-0 z-[-2] h-auto w-screen bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]";
+  const { theme } = useTheme();
+  console.log("Theme, ", theme);
+
+  const [bgtheme, setbgtheme] = useState(
+    theme === "dark" ? darkTheme : lightTheme
+  );
+
+  useEffect(() => {
+    setbgtheme(theme === "dark" ? darkTheme : lightTheme);
+  }, [theme]);
+
+  // const [color, setColor] = useState("#ffffff");
+
+  // useEffect(() => {
+  //   setColor(theme === "dark" ? "#ffffff" : "#000000");
+  // }, [theme]);
+
   return (
-    <main>
-      <h1>MY AI SHOTS</h1>
+    <main className={bgtheme}>
+      {/* metors */}
+      {/* <div className="relative flex h-[500px] w-full flex-col items-center justify-center overflow-hidden rounded-lg border bg-background md:shadow-xl"> */}
+      <Meteors number={100} />
+
+      {/* </div> */}
+
+      {/* <div>
+        <NavigationMenu />
+      </div> */}
+      <ModeToggle />
+      <div className="grid grid-cols-1 md:grid-cols-2">
+        <LandingHeader />
+        <ImageTree />
+      </div>
+
+      {/* <div className="relative flex h-[500px] w-full flex-col items-center justify-center overflow-hidden md:shadow-xl">
+        <Particles
+          className="absolute"
+          quantity={100}
+          ease={80}
+          color={color}
+          refresh
+        />
+      </div> */}
     </main>
   );
 }
---
+++ src/components/landing/header.tsx
@@ -0,0 +1,49 @@
+"use client";
+import React from "react";
+import { Button } from "@/components/ui/button";
+import BoxReveal from "@/components/ui/box-reveal";
+import ShimmerButton from "../ui/shimmer-button";
+
+export function LandingHeader() {
+  return (
+    <div className="w-full items-center justify-center overflow-hidden pt-8 p-4">
+      <BoxReveal boxColor={"#5046e6"} duration={0.5}>
+        <p className="text-[4rem] md:text-[5.5rem] font-semibold text-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text text-transparent relative z-20">
+          Inprime AI<span className="text-[#5046e6]">.</span>
+        </p>
+      </BoxReveal>
+
+      <BoxReveal boxColor={"#5046e6"} duration={0.5}>
+        <h2 className="mt-[.5rem] text-[1rem]">
+          UI library for{" "}
+          <span className="text-[#5046e6]">Design Engineers</span>
+        </h2>
+      </BoxReveal>
+
+      <BoxReveal boxColor={"#5046e6"} duration={0.5}>
+        <div className="mt-6">
+          <p>
+            -&gt; 20+ free and open-source animated components built with
+            <span className="font-semibold text-[#5046e6]"> React</span>,
+            <span className="font-semibold text-[#5046e6]"> Typescript</span>,
+            <span className="font-semibold text-[#5046e6]"> Tailwind CSS</span>,
+            and
+            <span className="font-semibold text-[#5046e6]"> Framer Motion</span>
+            . <br />
+            -&gt; 100% open-source, and customizable. <br />
+          </p>
+        </div>
+      </BoxReveal>
+
+      <BoxReveal boxColor={"#5046e6"} duration={0.5}>
+        <div className="z-10 flex items-center justify-center mt-10">
+          <ShimmerButton className="shadow-2xl">
+            <span className="whitespace-pre-wrap text-center text-sm font-medium leading-none tracking-tight text-white dark:from-white dark:to-slate-900/10 lg:text-lg">
+              Shimmer Button
+            </span>
+          </ShimmerButton>
+        </div>
+      </BoxReveal>
+    </div>
+  );
+}
