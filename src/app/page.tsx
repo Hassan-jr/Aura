54659dd72c2ebfed2c6a231e998514702e102268
Hassan-jr <abdiladifhassan115@gmail.com>
2024-10-16 14:28:13 +0300

    v0.2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,13 @@
-"use client";
 import { LandingHeader } from "@/components/landing/header";
 import { ImageTree } from "@/components/landing/imgtree";
-import { ModeToggle } from "@/customui/themebtn";
+import { LandingNav } from "@/components/landing/nav";
 
-import { useTheme } from "next-themes";
-import { useEffect, useState } from "react";
 
 export default function Home() {
-  const lightTheme =
-    "absolute top-0 -z-10 h-auto w-screen bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]";
-  const darkTheme =
-    "absolute top-0 z-[-2] h-auto w-screen bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]";
-  const { theme } = useTheme();
-  console.log("Theme, ", theme);
-
-  const [bgtheme, setbgtheme] = useState(
-    theme === "dark" ? darkTheme : lightTheme
-  );
-
-  useEffect(() => {
-    setbgtheme(theme === "dark" ? darkTheme : lightTheme);
-  }, [theme]);
 
   return (
-    <main className={bgtheme}>
-      <ModeToggle />
+    <main>
+      <LandingNav />
       <div className="grid grid-cols-1 md:grid-cols-2">
         <LandingHeader />
         <ImageTree />
---
+++ src/components/landing/nav.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+
+import Link from "next/link";
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+import { Card } from "@/components/ui/card";
+
+export function LandingNav() {
+  return (
+    <div className="flex flex-col w-full max-w-2xl lg:max-w-3xl xl:max-w-4xl mx-auto py-2 px-1">
+      <Card className="sticky top-0 z-50">
+        <Tabs
+          value=""
+          className="w-full my-1 flex justify-center flex-nowrap rounded-full relative"
+        >
+          <TabsList className="w-full max-w-2xl lg:max-w-3xl xl:max-w-4xl mx-auto px-1 h-auto rounded-sm bg-transparent gap-1">
+            {/* Features */}
+            <Link href="#" prefetch={false}>
+              <TabsTrigger
+                value="feaatures"
+                className="flex flex-col flex-nowrap justify-center align-middle bg-gradient-to-r from-indigo-500 to-purple-500 text-white dark:text-white py-1"
+              >
+                Features
+              </TabsTrigger>
+            </Link>
+            {/* Pricing */}
+            <Link href="#" prefetch={false}>
+              <TabsTrigger
+                value="feaatures"
+                className="flex flex-col flex-nowrap justify-center align-middle bg-gradient-to-r from-indigo-500 to-purple-500 text-white dark:text-white py-1"
+              >
+                Pricing
+              </TabsTrigger>
+            </Link>
+            {/* Testmonials */}
+            <Link href="#" prefetch={false}>
+              <TabsTrigger
+                value="feaatures"
+                className="flex flex-col flex-nowrap justify-center align-middle bg-gradient-to-r from-indigo-500 to-purple-500 text-white dark:text-white py-1"
+              >
+                Testmoials
+              </TabsTrigger>
+            </Link>
+            {/* App */}
+            <Link href="/posts" prefetch={false}>
+              <TabsTrigger
+                value="feaatures"
+                className="flex flex-col flex-nowrap justify-center align-middle bg-gradient-to-r from-indigo-500 to-purple-500 text-white dark:text-white py-1"
+              >
+                App
+              </TabsTrigger>
+            </Link>
+          </TabsList>
+        </Tabs>
+      </Card>
+    </div>
+  );
+}
