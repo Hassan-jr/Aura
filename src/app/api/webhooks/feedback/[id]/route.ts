246f8d38fc97ac1c8961a73c9764d6c81592c0ed
nomapos <abdiladifhassan115@gmail.com>
2024-12-14 16:51:54 +0300

    runpod 4

--- src/app/api/webhooks/feedback/[id]/route.ts
+++ src/app/api/webhooks/feedback/[id]/route.ts
@@ -1,12 +1,49 @@
+// import { NextRequest, NextResponse } from 'next/server';
+// import { connect } from '@/db';
+// import { Feedback } from '@/modals/feedback.modal';
+
+// export async function POST(
+//   request: NextRequest,
+//   { params }: { params: { id: string } }
+// ) {
+//   const id = params.id;
+
+//   try {
+//     // Connect to the database
+//     await connect();
+
+//     // Parse the request body
+//     const data = await request.json();
+
+//     // Update the Feedback document
+//     if (data.status === "COMPLETED") {
+//       const polarity = data.output.polarity[0];
+//       const emotion = data.output.emotion[0];
+
+//       await Feedback.findByIdAndUpdate(
+//         id,
+//         { polarity, emotion }
+//       );
+//     }
+
+//     // Return a 200 status to acknowledge successful receipt
+//     return NextResponse.json({ message: 'Webhook received successfully' }, { status: 200 });
+//   } catch (error) {
+//     console.error('Error processing webhook:', error);
+//     return NextResponse.json({ message: 'Error processing webhook' }, { status: 500 });
+//   }
+// }
+
+
 import { NextRequest, NextResponse } from 'next/server';
 import { connect } from '@/db';
 import { Feedback } from '@/modals/feedback.modal';
 
 export async function POST(
-  request: NextRequest,
-  { params }: { params: { id: string } }
+  request: NextRequest, 
+  context: { params: { id: string } }
 ) {
-  const id = params.id;
+  const { id } = context.params;
 
   try {
     // Connect to the database
@@ -33,4 +70,3 @@ export async function POST(
     return NextResponse.json({ message: 'Error processing webhook' }, { status: 500 });
   }
 }
-
