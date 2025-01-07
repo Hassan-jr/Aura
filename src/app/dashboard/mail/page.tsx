1037962b372ea0a58d206b7fe063828aac78e66e
nomapos <abdiladifhassan115@gmail.com>
2025-01-07 13:10:16 +0300

    agent v10

--- src/app/dashboard/mail/page.tsx
+++ src/app/dashboard/mail/page.tsx
@@ -6,8 +6,9 @@ import { Mail } from "./components/mail";
 import FetchEmails from "@/lib/fetchEmail";
 
 export default async function MailPage() {
-  const layout = cookies().get("react-resizable-panels:layout:mail");
-  const collapsed = cookies().get("react-resizable-panels:collapsed");
+  const cookieStore = await cookies()
+  const layout = cookieStore.get("react-resizable-panels:layout:mail");
+  const collapsed = cookieStore.get("react-resizable-panels:collapsed");
 
   const defaultLayout = layout ? JSON.parse(layout.value) : undefined;
   const defaultCollapsed = collapsed ? JSON.parse(collapsed.value) : undefined;
